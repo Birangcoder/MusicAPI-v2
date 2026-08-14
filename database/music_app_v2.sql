48a08acbc60761d498c9895ea5428fa6390e0aa0
Birang Pawala <birangmodi9327@gmail.com>
2026-08-14 17:11:29 +0530

    update db

--- database/music_app_v2.sql
+++ database/music_app_v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 14, 2026 at 02:30 AM
+-- Generation Time: Aug 14, 2026 at 09:59 AM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
